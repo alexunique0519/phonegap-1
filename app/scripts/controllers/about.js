39dddbfda96650a9658753b02236750d3b21d86a
zahid <zahid4688@gmail.com>
2014-11-27 16:00:53 -0500

    Project uploaded

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name phonegapApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the phonegapApp
+ */
+angular.module('phonegapApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name phonegapApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the phonegapApp
+ */
+angular.module('phonegapApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
