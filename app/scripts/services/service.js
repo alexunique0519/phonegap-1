e040d1290878fb0357afe85430ebe6b5da017393
zahid <zahid4688@gmail.com>
2014-11-28 16:40:41 -0500

    service added

---
+++ app/scripts/services/service.js
@@ -0,0 +1,23 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name phonegapApp.service
+ * @description
+ * # service
+ * Service in the phonegapApp.
+ */
+var App = angular.module('phonegapApp')
+
+
+App.factory('news', ['$http', function($http) {
+    return {
+        getAllData: function() {
+            var promise = $http.get(baseURL + "/query_temp.php");
+           
+
+            return promise;
+        }
+
+    };
+}]);
